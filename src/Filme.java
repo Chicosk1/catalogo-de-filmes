4504318feef5b47e9da3fb802b10ec8093542a9c
Gabriel Cichocki <gabrielsamudiocichocki@gmail.com>
2024-09-09 13:13:22 -0300

    Adicionando métodos para a classe Filme

--- src/Filme.java
+++ src/Filme.java
@@ -1,5 +1,27 @@
 public class Filme {
+    
     String nome;
-    boolean incluidoNoPlano, avaliacao;
-    int totalDeAvaliacoes, duracaoEmMinutos, anoDeLancamento;
+    boolean incluidoNoPlano;
+    int totalDeAvaliacoes, duracaoEmMinutos, anoDeLancamento, somaDasAvaliacoes; 
+
+    void exibeFichaTecnica(){
+
+        System.out.println("Nome do filme:" + nome);
+        System.out.println("Ano de lançamento:" + anoDeLancamento);
+   
+    }
+
+    void avaliarFilme(double notaFilme) {
+
+        somaDasAvaliacoes += notaFilme;
+        totalDeAvaliacoes++;
+
+    }
+
+    double retornarMediaDasAvaliacoes() {
+
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) throws Exception {
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "O Poderoso Chefão";
+        meuFilme.anoDeLancamento = 1970;
+        meuFilme.duracaoEmMinutos = 180;
+
+        meuFilme.exibeFichaTecnica();
+
+        meuFilme.avaliarFilme(8);
+        meuFilme.avaliarFilme(9);
+        meuFilme.avaliarFilme(10);
         
+        System.out.println(meuFilme.somaDasAvaliacoes);
+        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println(meuFilme.retornarMediaDasAvaliacoes());
+
     }
 }
