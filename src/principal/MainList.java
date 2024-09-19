e82567ceaf433e76cdcd31ce629db3a2f058e8c9
Chicosk1 <168223635+Chicosk1@users.noreply.github.com>
2024-09-19 08:38:29 -0300

    Update MainList.java

--- src/principal/MainList.java
+++ src/principal/MainList.java
@@ -11,21 +11,24 @@ import java.util.List;
 
 public class MainList {
     public static void main(String[] args) {
+        
         Filme meuFilme = new Filme();
-        meuFilme.avaliarFilme(9);
         Filme outroFilme = new Filme();
-        outroFilme.avaliarFilme(6);
-        var filmeDoPaulo = new Filme();
-        filmeDoPaulo.avaliarFilme(10);
         Serie lost = new Serie();
+        var filmeDoGabriel = new Filme();
 
-        Filme f1 = filmeDoPaulo;
+        meuFilme.avaliarFilme(9);
+        outroFilme.avaliarFilme(6);
+        filmeDoGabriel.avaliarFilme(10);
 
         ArrayList<Titulo> lista = new ArrayList<>();
-        lista.add(filmeDoPaulo);
+
+        lista.add(filmeDoGabriel);
         lista.add(meuFilme);
         lista.add(outroFilme);
         lista.add(lost);
+
+        // Loop percorrendo a lista
         for (Titulo item: lista) {
             System.out.println(item.getNome());
             if (item instanceof  Filme filme && filme.getClassificacao() > 2) {
@@ -34,17 +37,22 @@ public class MainList {
         }
 
         List<String> buscaPorArtista = new LinkedList<>();
+
         buscaPorArtista.add("Adam Sandler");
-        buscaPorArtista.add("Paulo");
-        buscaPorArtista.add("Jacqueline");
+        buscaPorArtista.add("Gabriel");
+        buscaPorArtista.add("Beatriz");
+
         System.out.println(buscaPorArtista);
 
         Collections.sort(buscaPorArtista);
+
         System.out.println("Depois da ordenação");
         System.out.println(buscaPorArtista);
+
         System.out.println("Lista de titulos ordenados");
         Collections.sort(lista);
         System.out.println(lista);
+
         lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
         System.out.println("Ordenando por ano");
         System.out.println(lista);
