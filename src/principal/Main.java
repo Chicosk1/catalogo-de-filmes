0c1700f8ede5596f09de175fb7c1adedb1420b7f
Chicosk1 <168223635+Chicosk1@users.noreply.github.com>
2024-09-19 08:38:16 -0300

    Update Main.java

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -1,23 +1,31 @@
 package principal;
+
 import modelos.*;
 import calculos.*;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) throws Exception {
+
         Filme meuFilme = new Filme();
+
         meuFilme.setNome("O poderoso chefão");
         meuFilme.setAnoDeLancamento(1970);
         meuFilme.setDuracaoEmMinutos(180);
         System.out.println("Duração do filme: " + meuFilme.getDuracaoEmMinutos());
 
         meuFilme.exibeFichaTecnica();
+
         meuFilme.avaliarFilme(8);
         meuFilme.avaliarFilme(5);
         meuFilme.avaliarFilme(10);
         System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+
         System.out.println(meuFilme.retornarMediaDasAvaliacoes());
 
         Serie minhaSerie = new Serie();
+
         minhaSerie.setNome("Lost");
         minhaSerie.setAnoDeLancamento(2000);
         minhaSerie.exibeFichaTecnica();
@@ -27,20 +35,25 @@ public class Main {
         System.out.println("Duração para maratonar Lost: " + minhaSerie.getDuracaoEmMinutos());
 
         Filme outroFilme = new Filme();
+
         outroFilme.setNome("Avatar");
         outroFilme.setAnoDeLancamento(2023);
         outroFilme.setDuracaoEmMinutos(200);
 
         CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+
         calculadora.incluirTitulo(meuFilme);
         calculadora.incluirTitulo(outroFilme);
         calculadora.incluirTitulo(minhaSerie);
+
         System.out.println(calculadora.getTempoTotal());
 
         FiltroRecomendacao filtro = new FiltroRecomendacao();
+
         filtro.filtrarFilme(meuFilme);
 
         Episodio episodio = new Episodio();
+        
         episodio.setNumero(1);
         episodio.setSerie(minhaSerie);
         episodio.setTotalVisualizacoes(300);
